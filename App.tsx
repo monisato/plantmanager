5b6451750076904ab538f13ba00e35d7d89c494e
monisato <monicasakane@gmail.com>
2021-04-20 11:31:34 -0300

    aula 01 47min

--- App.tsx
+++ App.tsx
@@ -1,21 +1,29 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View, StyleSheet } from 'react-native';
 
-export default function App() {
+export default function App(){
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={ style.container }>
+      <Text>
+        Olá!
+      </Text>
+      <Text>
+        NLW#5
+      </Text>
     </View>
-  );
+  )
 }
 
-const styles = StyleSheet.create({
+const style = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
-  },
-});
+    alignItems: 'center'
+  }
+})
+
+
+// fragment: 
+// caso na utilização de multiplos tags repetidos usar estes em volta => <> </>
+// ou usar <View> </View>, <Fragment> </Fragment>
+// import React, { Fragment } from 'react';
