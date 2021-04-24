2a82e82313fcb11217dedc138ab1b92cf8892bce
monisato <monicasakane@gmail.com>
2021-04-24 19:00:28 -0300

    aula 3 terminado

--- src/pages/PlantSelect.tsx
+++ src/pages/PlantSelect.tsx
@@ -3,13 +3,16 @@ import {
     View,
     Text,
     StyleSheet,
-    FlatList
+    FlatList,
+    ActivityIndicator
 } from'react-native';
+
 import { EnvironmentButton } from '../components/EnvironmentButton';
-
 import { Header } from '../components/Header';
-import api from '../services/api';
+import { PlantCardPrimary } from '../components/PlantCardPrimary';
+import { Load } from '../components/Load';
 
+import api from '../services/api';
 import colors from '../styles/colors';
 import fonts from '../styles/fonts';
 
@@ -19,13 +22,73 @@ interface EnvironmentProps {
     title: string;
 }
 
+interface PlantProps {
+    id: string;
+    name: string;
+    about: string;
+    water_tips: string;
+    photo: string;
+    environments: [string];
+    frequency: {
+        times: number;
+        repeat_every: string;
+    }
+}
+
 export function PlantSelect(){
     const [environments, setEnvironments] = useState<EnvironmentProps[]>([]);
+    const [plants, setPlants] = useState<PlantProps[]>([]);
+    const [filteredPlants, setFilteredPlants] = useState<PlantProps[]>([]);
+    const [environmentSelected, setEnvironmentSelected] = useState('all');
+    const [loading, setLoading] = useState(true);
+
+    const [page, setPage] = useState(1);
+    const [loadingMore, setLoadingMore] = useState(false);
+    const [loadedAll, setLoadedAll] = useState(false);
+
+    function handleEnvironmentSelected(environment: string) {
+        setEnvironmentSelected(environment);
+
+        if(environment === 'all')
+        return setFilteredPlants(plants);
+
+        const filtered = plants.filter(plant => 
+            plant.environments.includes(environment)
+        );
+
+        setFilteredPlants(filtered);
+    }
+
+    async function fetchPlants(){
+        const { data } = await api.get(`plants?_sort=name&_order=asc&_page=${page}&_limit=8`); // await, aguarda carregar dados antes de mostrar pro usuário
+        
+        if(!data)
+        return setLoading(true);
+
+        if(page > 1) {
+            setPlants(oldValue => [... oldValue, ...data])
+            setFilteredPlants(oldValue => [... oldValue, ...data])
+        } else {
+            setPlants(data);
+            setFilteredPlants(data);
+        }
+        
+        setLoading(false);
+        setLoadingMore(false);
+    }
+    
+    function handleFetchMore(distance: number) {
+        if(distance < 1)
+        return;
+        setLoadingMore(true);
+        setPage(oldValue => oldValue + 1);
+        fetchPlants();
+    }
 
     // carregar antes dos elementos da interface, função async dentro do useEffect pois não é possivel colocar função async no useEffect
     useEffect(() => {
-        async function fetchEnvironment(){
-            const { data } = await api.get('plants_environments'); // await, aguarda carregar dados antes de mostrar pro usuário
+        async function fetchEnvironment(){ // ?_sort=title&_order=asc para ordenar por ordem alfabetica
+            const { data } = await api.get('plants_environments?_sort=title&_order=asc'); // await, aguarda carregar dados antes de mostrar pro usuário
             setEnvironments([
                 {
                     key:'all',
@@ -37,6 +100,13 @@ export function PlantSelect(){
 
         fetchEnvironment();
     }, [])
+
+    useEffect(() => {
+        fetchPlants();
+    }, [])
+
+    if(loading)
+    return <Load />
 
     return (
         <View style={styles.container}>
@@ -56,6 +126,8 @@ export function PlantSelect(){
                     renderItem={({ item }) => (
                         <EnvironmentButton 
                             title={item.title}
+                            active={item.key === environmentSelected}
+                            onPress={() => handleEnvironmentSelected(item.key)}
                         />
                     )}
                     horizontal
@@ -63,6 +135,21 @@ export function PlantSelect(){
                     contentContainerStyle={styles.environmentList}
                 />
             </View>
+
+            <View style={styles.plants}>
+                <FlatList 
+                    data={filteredPlants}
+                    renderItem={({ item }) => (
+                        <PlantCardPrimary data={item}/>
+                    )}
+                    showsVerticalScrollIndicator={false}
+                    numColumns={2}
+                    onEndReachedThreshold={0.1}
+                    onEndReached={({ distanceFromEnd }) => handleFetchMore(distanceFromEnd)}
+                    ListFooterComponent={loadingMore ? <ActivityIndicator color={colors.green}/> : <></>}
+                />
+            </View>
+
         </View>
     )
 }
@@ -94,5 +181,11 @@ const styles = StyleSheet.create({
         paddingBottom: 5,
         marginLeft: 32,
         marginVertical: 32
-    }
+    },
+    plants: {
+        flex: 1,
+        paddingHorizontal: 32,
+        justifyContent: 'center'
+    },
+    
 });
--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://192.168.100.38:3333'
+    baseURL: 'http://10.0.0.143:3333'
+    // baseURL: 'http://192.168.100.38:3333'
 });
 
 export default api;
